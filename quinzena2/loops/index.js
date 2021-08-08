b23b29c5f99052e83801011434a584e7f82e44f9
PriscilaOSousa <86696726+PriscilaOSousa@users.noreply.github.com>
2021-08-08 20:36:04 -0300

    exercicio-aula09-atualizado

--- quinzena2/loops/index.js
+++ quinzena2/loops/index.js
@@ -14,14 +14,51 @@
 
 // Exercícios de escrita de código
 // 1-) a)
+const quantidadeBichinhos = Number(prompt("Quantos bichinhos de estimação você tem?"))
+if(quantidadeBichinhos > 0) {
+    const nomes = []
+    for(let i = 0; i < quantidadeBichinhos; i++) {
+        nomes.push(prompt("Diga o nome dos seus bichinhos"))
+    } 
+console.log(nomes)
+} else { 
+    console.log("Que pena! Você pode adotar um pet!")
+}
 
-for(let nomePet = 0; nomePet >= 0; nomePet++) {
-    const nome = Number(prompt("Quantos animais de estimação você tem?"))
-console.log("Que pena! Você pode adotar um pet!")
-nomePet = nome
-} if(nomePet >= 0){
-    console.log("Diga o nome do seu pet")
-} 
+// 2-) a) b) C) d)
+
+// Este array será usado para exemplificar as respostas de todos os itens
+const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+// a)
+
+for(let lista of array) {
+    console.log(lista)
+}
+
+// b)
+
+for(let lista of array) {
+    console.log(lista / 10) 
+}
+
+// c)
+
+let novoArray = []
+for(let lista of array) {
+    if(lista % 2 === 0){
+        novoArray.push(lista)
+    }
+ } 
+ console.log(novoArray) 
+
+// d)
+
+
+
+let respostaElemento = "O elemento do index";
+
+console.log(`${s} ${index} é ${variavel}`);
 
 
 
